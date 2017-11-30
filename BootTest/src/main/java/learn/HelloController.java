8611fd3b0c17b720e78b9d17785c9324bfab7de7
Arvind Yadav <arvind_yadav@gymboree.com>
2017-11-30 15:43:05 -0800

    done

--- BootTest/src/main/java/learn/HelloController.java
+++ BootTest/src/main/java/learn/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot test completed! "; 
+        return "Greetings from Spring Boot! "; 
     }
 
 }
