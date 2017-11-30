31367446667996ddf30614876cda391068164de3
arvindyadav82 <34139619+arvindyadav82@users.noreply.github.com>
2017-11-30 15:47:34 -0800

    Update HelloController.java

--- BootTest/src/main/java/learn/HelloController.java
+++ BootTest/src/main/java/learn/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot Arvind test! "; 
+        return "Greetings from Spring Boot Arvind test Done! "; 
     }
 
 }
